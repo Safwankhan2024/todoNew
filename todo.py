96feca0ed3d37fde729fc17f76a9cb48b336ce3d
Safwankhan2024 <sgr-2009-4479@cpsp.edu.pk>
2025-08-08 16:58:56 +0300

    Initial commit: todo.py

---
+++ todo.py
@@ -0,0 +1,19 @@
+#print("Hello, To-Do App!")
+#a list to store our todo. each task is a dictionary.
+tasks = [
+    {"task" : "read a book", "completed":False},
+    {"task" : "do some code", "completed":True}
+        ]
+def display_tasks():
+    print("---your todo list---")
+    if not tasks:
+        print("you have no tasks")
+    else:
+        for index, task in enumerate(tasks, start=1):
+            status_icon = "|/" if task["completed"] else "||"
+            print(f"{index},{status_icon}){task['task']}")
+    print(".................................................")
+
+if __name__ == "__main__":
+    display_tasks()
+            
